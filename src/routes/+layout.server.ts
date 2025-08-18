49e1e45730cdacf8ecdc8933e8b38795d6ca8db3
Kevin <50965376+tgodkev@users.noreply.github.com>
2025-08-18 14:19:43 -0400

    build issue fixed

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,14 +1,15 @@
 export async function load({ locals, url }) {
-	const currentRoute = url.pathname;
-	const siteSettings = await locals.pb
-		.collection('globals')
-		.getFirstListItem(`slug="siteSettings"`, { expand: 'menu, socials, globals, globals.pages' });
+  const currentRoute = url.pathname;
+  const siteSettings = await locals.pb
+    .collection('globals')
+    .getFirstListItem(`slug="siteSettings"`, { expand: 'menu, socials, globals, globals.pages' });
 
-	return {
-		siteSettings,
-		currentRoute
-	};
+  return {
+    siteSettings,
+    currentRoute
+  };
 }
 
 export const trailingSlash = 'always';
-export const prerender = true;
+export const ssr = true
+export const csr = true
